84ca18276a9344565679699e58202ab4dbd7d178
Harsha <harsha.parasar16@gmail.com>
2023-11-09 17:31:17 +0530

    second commit

---
+++ secondfile.js
@@ -0,0 +1,13 @@
+let a = 8;
+let b = 10;
+
+console.log('x + y = ',x + y);
+
+console.log('x - y = ',x - y);
+
+console.log('x * y = ',x * y);
+
+console.log('x / y = ',x / y);
+
+console.log('x % y = ',x % y);
+
