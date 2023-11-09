4e2d1b0dfd8b8f38a05b9dfa5631c4557240c0ce
Harsha <harsha.parasar16@gmail.com>
2023-11-09 17:31:57 +0530

    third commit

---
+++ third file.js
@@ -0,0 +1,23 @@
+let Num1 = 20;
+let Num2 = 18;
+if (Num1>Num2){
+    console.log("Num1 is greater");
+}
+else{
+    console.log("Num2 is greater");
+}
+
+let i=0;
+while(i<5){
+    i=i+1;
+    console.log("While loop iteration", i);
+}
+for(i=0; i<5; i++){
+    console.log("for loop iteration", i);
+}
+
+let t=0;
+do{
+    t=t+1;
+    console.log("do-while loop iteration", t);
+}while(t<5)
